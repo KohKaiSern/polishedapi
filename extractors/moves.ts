bc721a6460ec51332d72871f9c5a27afd3d72147
Koh Kai Sern <kaisern.koh@gmail.com>
2026-09-22 23:22:48 +0800

    Finish Move Extraction

--- extractors/moves.ts
+++ extractors/moves.ts
@@ -19,7 +19,31 @@ function extractAttrs(ATTRS: string[]): {
   effectChance: number;
   category: string;
 }[] {
-  return []
+  const data = [];
+  let index = 1;
+  for (let lineNo = 0; lineNo < ATTRS.length; lineNo++) {
+    if (!ATTRS[lineNo].startsWith('move')) continue;
+    const match = ATTRS[lineNo].match(/move (\w+),\s+(\w+),\s+(\d+),\s+(\w+),\s+(-?\d+),\s+(\d+).\s+(\d+),\s+(\w+)/)!;
+    const effect: string = match[2];
+    const basePower: number = parseInt(match[3]);
+    const type: string = match[4];
+    const accuracy: number = parseInt(match[5]);
+    const powerPoints: number = parseInt(match[6]);
+    const effectChance: number = parseInt(match[7]);
+    const category: string = match[8];
+    data.push({
+      index,
+      effect,
+      basePower,
+      type,
+      accuracy,
+      powerPoints,
+      effectChance,
+      category
+    });
+    index++;
+  }
+  return data;
 }
 
 const moves: Split<Move> = {
@@ -40,7 +64,7 @@ for (const PF of ['polished', 'faithful'] as const) {
   const consts = extractConsts(files[0][PF]);
   const names = extractNames(files[1][PF], 1);
   const descs = extractDescs(files[2][PF], 1);
-  const attrs = extractAttrs(files[3][PF])
+  const attrs = extractAttrs(files[3][PF]);
   moves[PF] = consolidate<Move>(consts, names, descs, attrs)
 }
 
